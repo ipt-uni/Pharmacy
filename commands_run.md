75ce657d51632ee25e9653448cfe59756443ece5
jaber-tebyan <jabertebyan@gmail.com>
2026-06-25 12:14:33 +0100

    chore/fix: applied migrations, fixed grants of user

--- commands_run.md
+++ commands_run.md
@@ -4,4 +4,7 @@ dotnet new webapp --auth Individual -uld
 # Mysql
 dotnet remove package Microsoft.EntityFrameworkCore.SqlServer
 dotnet add package Microting.EntityFrameworkCore.MySql # For latest dotnet 10 and support of mariadb
+
+dotnet ef migrations add InitialMysqlCreate
+
 ```
--- mysql_code.sql
+++ mysql_code.sql
@@ -1,4 +1,5 @@
 create database pharmacy_webdev;
 create user 'webdev_pharmacy_user'@'%' identified by 'webdev_pharmacy_user';
 grant create,drop,alter,insert,update,select,delete on pharmacy_webdev.* to webdev_pharmacy_user;
+grant index on pharmacy_webdev.* to webdev_pharmacy_user;
 
