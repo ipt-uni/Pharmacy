5aa70fc28e5c74411d27f217b68ee403218842ab
jaber-tebyan <jabertebyan@gmail.com>
2026-06-25 18:06:30 +0100

    fix: fix models not accessible

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,9 +6,9 @@ namespace pharmacy.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
-    DbSet<Company> Companies { get; set; } = null!;
-    DbSet<Medicine> Medicines { get; set; } = null!; DbSet<Supplier> Suppliers { get; set; } = null!;
-    DbSet<Cart> Carts { get; set; } = null!;
-    DbSet<CartItem> CartItems { get; set; } = null!;
-    DbSet<Payment> Payments { get; set; } = null!;
+    public DbSet<Company> Companies { get; set; } = null!;
+    public DbSet<Medicine> Medicines { get; set; } = null!; DbSet<Supplier> Suppliers { get; set; } = null!;
+    public DbSet<Cart> Carts { get; set; } = null!;
+    public DbSet<CartItem> CartItems { get; set; } = null!;
+    public DbSet<Payment> Payments { get; set; } = null!;
 }
