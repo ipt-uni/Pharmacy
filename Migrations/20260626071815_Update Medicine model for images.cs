5e17ad79291ab8b8836623344cd3fc66851fbe4b
jaber-tebyan <jabertebyan@gmail.com>
2026-06-26 08:19:35 +0100

    chore: add and apply migrations

---
+++ Migrations/20260626071815_Update Medicine model for images.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pharmacy.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMedicinemodelforimages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "imageSrc",
+                table: "Medicines",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imageSrc",
+                table: "Medicines");
+        }
+    }
+}
