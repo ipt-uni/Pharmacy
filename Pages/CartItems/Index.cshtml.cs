84d7a54542db4f267acdba367b2b4a53dc7a2778
jaber-tebyan <jabertebyan@gmail.com>
2026-06-26 21:12:57 +0100

    chore: scaffold crud pages for CartItem

---
+++ Pages/CartItems/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using pharmacy.Data;
+using pharmacy.Data.Models;
+
+namespace pharmacy.Pages.CartItems
+{
+    public class IndexModel : PageModel
+    {
+        private readonly pharmacy.Data.ApplicationDbContext _context;
+
+        public IndexModel(pharmacy.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CartItem> CartItem { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            CartItem = await _context.CartItems
+                .Include(c => c.Cart)
+                .Include(c => c.Medicine).ToListAsync();
+        }
+    }
+}
