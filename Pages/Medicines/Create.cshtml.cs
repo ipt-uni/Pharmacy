e6f37180b161f1c0b764d6c62c61ac93e1ad4260
jaber-tebyan <jabertebyan@gmail.com>
2026-06-26 13:04:08 +0100

    feat: add image saving in medicine

--- Pages/Medicines/Create.cshtml.cs
+++ Pages/Medicines/Create.cshtml.cs
@@ -17,9 +17,19 @@ namespace pharmacy.Pages.Medicines
 
         private readonly pharmacy.Data.ApplicationDbContext _context;
 
-        public CreateModel(pharmacy.Data.ApplicationDbContext context)
+        /// <summary>
+        /// To access the web host environment for determining uploaded
+        /// image file should be saved
+        /// </summary>
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public CreateModel(
+            pharmacy.Data.ApplicationDbContext context,
+            IWebHostEnvironment webHostEnvironment
+        )
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult OnGet()
@@ -38,9 +48,92 @@ namespace pharmacy.Pages.Medicines
             {
                 return Page();
             }
+            /// 1. is to check whether there is a file upload or image url.
+            /// They can't be both
+            /// 2. if it is file:
+            ///    2.1 if the file is an image
+            ///        Then specify it's name
+            ///        assign the full image path to Medicine object
+            ///        save the file
+            ///     otherwise:
+            ///       throw an error indicating that the file is not an image
+            /// 3. if it is url:
+            ///    just set it and don't check much
+            /// 4. there can't be both image url and image file.
+            if (ImageFile != null && !string.IsNullOrEmpty(ImageUrl))
+            {
+                ModelState.AddModelError(
+                    "ImageFile",
+                    "You can't upload an image and an image url at the same time"
+                );
+                return Page();
+            }
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                if (
+                    !(ImageFile.ContentType == "image/jpeg" || ImageFile.ContentType == "image/png")
+                )
+                {
+                    ModelState.AddModelError("ImageFile", "The file is not an image");
+                    return Page();
+                }
+                string imageName =
+                    Guid.NewGuid().ToString()
+                    + Path.GetExtension(ImageFile.FileName).ToLowerInvariant();
+                // save the image file to the server
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                string imageDir = Path.Combine(wwwRootPath, "images");
+                string savePath = Path.Combine(imageDir, imageName);
+                Medicine.imageSrc = "/images/" + imageName;
+                try
+                {
+                    if (!Directory.Exists(imageDir))
+                    {
+                        Directory.CreateDirectory(imageDir);
+                    }
+                    // specify the path where to save the file
+                    using (var stream = new FileStream(savePath, FileMode.Create))
+                    {
+                        await ImageFile.CopyToAsync(stream);
+                    }
+                }
+                catch (Exception)
+                {
+                    // throw;
 
-            _context.Medicines.Add(Medicine);
-            await _context.SaveChangesAsync();
+                    // in production, you should log the exception
+                    // and show a user-friendly message
+
+                    ModelState.AddModelError(
+                        string.Empty,
+                        "An error occurred while creating the medicine. Please try again."
+                    );
+                    return Page();
+                }
+            }
+            if (!string.IsNullOrEmpty(ImageUrl))
+            {
+                Medicine.imageSrc = ImageUrl;
+            }
+
+            try
+            {
+                _context.Medicines.Add(Medicine);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                // throw;
+
+                // in production, you should log the exception
+                // and show a user-friendly message
+
+                ModelState.AddModelError(
+                    string.Empty,
+                    "An error occurred while creating the medicine. Please try again."
+                );
+                return Page();
+            }
 
             return RedirectToPage("./Index");
         }
