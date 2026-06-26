59ce070df717dbff523de25e0b06891e3caf22ab
jaber-tebyan <jabertebyan@gmail.com>
2026-06-26 23:44:39 +0100

    feat: add users to context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,11 +4,16 @@ using pharmacy.Data.Models;
 
 namespace pharmacy.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext(options)
 {
     public DbSet<Company> Companies { get; set; } = null!;
-    public DbSet<Medicine> Medicines { get; set; } = null!; DbSet<Supplier> Suppliers { get; set; } = null!;
+    public DbSet<Medicine> Medicines { get; set; } = null!;
+    DbSet<Supplier> Suppliers { get; set; } = null!;
     public DbSet<Cart> Carts { get; set; } = null!;
     public DbSet<CartItem> CartItems { get; set; } = null!;
     public DbSet<Payment> Payments { get; set; } = null!;
+    public DbSet<MyUser> MyUsers { get; set; } = null!;
+    public DbSet<Customer> Customers { get; set; } = null!;
+    public DbSet<NonCustomer> NonCustomers { get; set; } = null!;
 }
