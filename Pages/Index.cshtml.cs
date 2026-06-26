444dd594328a970778b6fdf5d03a1f8cc3173fad
jaber-tebyan <jabertebyan@gmail.com>
2026-06-26 20:27:32 +0100

    feat: add medicines grid in home

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using pharmacy.Data;
+using pharmacy.Data.Models;
 
 namespace pharmacy.Pages;
 
 public class IndexModel : PageModel
 {
+    ApplicationDbContext _context;
+
+    public IndexModel(ApplicationDbContext context)
+        : base()
+    {
+        _context = context;
+    }
+
+    public ICollection<Medicine> Medicines = new List<Medicine>();
+
     public void OnGet()
     {
-
+        Medicines = _context.Medicines.Include(m => m.Company).ToList();
     }
 }
