5f46540f2a46b66314d3b896c62a7dccb08dc393
jaber-tebyan <jabertebyan@gmail.com>
2026-06-26 21:08:12 +0100

    chore: applied migrations and updated database

---
+++ Migrations/20260626200744_Add UnitPrice To CartItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pharmacy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUnitPriceToCartItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "UnitPrice",
+                table: "CartItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitPrice",
+                table: "CartItems");
+        }
+    }
+}
