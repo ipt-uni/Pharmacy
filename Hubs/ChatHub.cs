0df4c0cb7bb1365296faed53a83194a38864c6c3
jaber-tebyan <jabertebyan@gmail.com>
2026-06-27 13:47:20 +0100

    feat: modify chathub

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,11 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace pharmacy.Hubs;
 
+[Authorize]
 public class ChatHub : Hub
 {
     public async Task SendMessage(string message)
     {
-        await Clients.All.SendAsync("ReceiveMessage", message);
+        var username = Context.User?.Identity?.Name ?? "Unknown";
+        Console.WriteLine($"Sending message to {username}: {message}");
+        await Clients.All.SendAsync("ReceiveMessage", username, message);
     }
 }
