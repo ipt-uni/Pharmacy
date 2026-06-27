d2b2d48ef0c536dd6f7b9de342cb9dc1d9bd3514
jaber-tebyan <jabertebyan@gmail.com>
2026-06-27 12:40:07 +0100

    feat: add hub

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace pharmacy.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
