6dcd6b45926cde8c40a9e878517ae93b784aa680
jaber-tebyan <jabertebyan@gmail.com>
2026-06-27 09:14:26 +0100

    chore: scaffold MedicineController

---
+++ Controllers/MedicineController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pharmacy.Data;
+using pharmacy.Data.Models;
+
+namespace pharmacy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicineController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MedicineController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Medicine
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Medicine>>> GetMedicine()
+        {
+            return await _context.Medicines.ToListAsync();
+        }
+
+        // GET: api/Medicine/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Medicine>> GetMedicine(int id)
+        {
+            var medicine = await _context.Medicines.FindAsync(id);
+
+            if (medicine == null)
+            {
+                return NotFound();
+            }
+
+            return medicine;
+        }
+
+        // PUT: api/Medicine/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMedicine(int id, Medicine medicine)
+        {
+            if (id != medicine.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(medicine).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MedicineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Medicine
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Medicine>> PostMedicine(Medicine medicine)
+        {
+            _context.Medicines.Add(medicine);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMedicine", new { id = medicine.Id }, medicine);
+        }
+
+        // DELETE: api/Medicine/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMedicine(int id)
+        {
+            var medicine = await _context.Medicines.FindAsync(id);
+            if (medicine == null)
+            {
+                return NotFound();
+            }
+
+            _context.Medicines.Remove(medicine);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MedicineExists(int id)
+        {
+            return _context.Medicines.Any(e => e.Id == id);
+        }
+    }
+}
