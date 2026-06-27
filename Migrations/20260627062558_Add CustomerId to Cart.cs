53594936b4b68b2b97c7a815ea9684368b5f6ca4
jaber-tebyan <jabertebyan@gmail.com>
2026-06-27 07:26:20 +0100

    chore: apply migrations

---
+++ Migrations/20260627062558_Add CustomerId to Cart.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pharmacy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerIdtoCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalCost",
+                table: "Carts");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalQuantity",
+                table: "Carts",
+                newName: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_CustomerId",
+                table: "Carts",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_MyUsers_CustomerId",
+                table: "Carts",
+                column: "CustomerId",
+                principalTable: "MyUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_MyUsers_CustomerId",
+                table: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_CustomerId",
+                table: "Carts");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerId",
+                table: "Carts",
+                newName: "TotalQuantity");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalCost",
+                table: "Carts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
